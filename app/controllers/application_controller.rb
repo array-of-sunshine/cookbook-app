33a20d0ce9686a76e3124e90cc9d3ebbd080e8b3
Brian <riceb53@gmail.com>
2018-10-15 18:55:19 -0500

    add one recipe action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def one_recipe_action
+    # how can i get some data from my db into my controller?
+    @recipe = Recipe.first
+    render "one_recipe_view.json.jbuilder"
+  end
 end
