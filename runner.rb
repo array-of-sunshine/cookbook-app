c2d7cde54c9e0f68483cd0d3e885c94469a16860
Brian <riceb53@gmail.com>
2018-10-15 19:20:37 -0500

    make runner.rb to test index and show actions

---
+++ runner.rb
@@ -0,0 +1,13 @@
+require 'unirest'
+# need to run 'gem install awesome_print' first
+require 'awesome_print'
+
+# # index
+# response = Unirest.get("http://localhost:3000/api/recipes")
+
+# ap response.body
+
+# # show
+# response = Unirest.get("http://localhost:3000/api/recipes/2")
+
+# ap response.body
