4591cf35fa958ce128c5ac3b99aa17fb5cb8072b
Brian <riceb53@gmail.com>
2018-11-13 21:28:41 -0600

    add callback example

--- public/recipes.js
+++ public/recipes.js
@@ -3,6 +3,9 @@ console.log('hello');
 // response = Unirest.get('localhost:3000/api/recipes')
 // p response.body
 
+// var response = axios.get('/api/recipes');
+// console.log(response.data) DOESN'T WORK
+
 axios.get('/api/recipes').then(function(response) {
   console.log(response.data);
   // add the data to the DOM
